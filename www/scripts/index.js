f4b15bafe536bde7befb3a72fba760fa4fea10bd
najeekitchens <najeekitchens@gmail.com>
2019-02-22 15:13:41 -0500

    Music and fade in transition

--- www/scripts/index.js
+++ www/scripts/index.js
@@ -8,15 +8,16 @@
     document.addEventListener('deviceready', onDeviceReady.bind(this), false);
     setTimeout(function () {
         if (document.location == "http://localhost:4400/index.html") {
+            document.body.className += 'fade-out';
             document.location.replace("http://localhost:4400/index2.html");
         }
     }, 2000);
-
+    
     function onDeviceReady() {
         // Handle the Cordova pause and resume events
-        document.addEventListener( 'pause', onPause.bind( this ), false );
-        document.addEventListener( 'resume', onResume.bind( this ), false );
-        
+        document.addEventListener('pause', onPause.bind(this), false);
+        document.addEventListener('resume', onResume.bind(this), false);
+
         // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
         var parentElement = document.getElementById('deviceready');
         var listeningElement = parentElement.querySelector('.listening');
